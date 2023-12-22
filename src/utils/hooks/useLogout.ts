17e4d609199d098e935a5cf276718c7b275b51c4
Fahad's Mac <mac@192.168.1.29>
2023-12-22 19:47:53 +0500

    rfd navigate change

--- src/utils/hooks/useLogout.ts
+++ src/utils/hooks/useLogout.ts
@@ -1,11 +1,14 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { resetUser } from "../../redux/reducers/userReducer";
+import { useNavigate } from "react-router";
 
 const useLogout = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const logout = () => {
     dispatch(resetUser());
+    navigate("/");
   };
 
   return { logout };
